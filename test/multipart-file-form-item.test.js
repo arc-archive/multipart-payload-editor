bb5421ac89116bb586d7b728228f9c5ceb3b5ff1
Carolina Wright <carolina.wright@mulesoft.com>
2021-06-10 18:03:07 -0300

    test: new field added

--- test/multipart-file-form-item.test.js
+++ test/multipart-file-form-item.test.js
@@ -2,7 +2,7 @@ import {
   fixture,
   assert,
   nextFrame,
-  html
+  html, aTimeout
 } from '@open-wc/testing';
 import * as sinon from 'sinon';
 import * as MockInteractions from '@polymer/iron-test-helpers/mock-interactions.js';
@@ -64,6 +64,15 @@ describe('<multipart-file-form-item>', function() {
       const node = element.shadowRoot.querySelector('input[type="file"]');
       const result = getComputedStyle(node).display.trim();
       assert.equal(result, 'none');
+    });
+
+    it('renders optional content type if hasFormData enabled', async () => {
+      const element = await basicFixture();
+      element.hasFormData = true
+      await aTimeout(100);
+
+      const node = element.shadowRoot.querySelector('.mime-selector');
+      assert.ok(node);
     });
   });
 
@@ -143,6 +152,68 @@ describe('<multipart-file-form-item>', function() {
       });
       assert.equal(spy.args[0][0].detail.value, value);
     });
+
+    it('should update file type if content type already defined', async () => {
+      const type = 'text/plain';
+      element.type = type
+      await aTimeout(100)
+
+      const spy = sinon.spy();
+      element.addEventListener('value-changed', spy);
+
+      const value = new Blob(['test'], {
+        type: 'application/json'
+      });
+      element._fileObjectChanged({
+        target: {
+          files: [value]
+        }
+      });
+
+      assert.equal(spy.args[0][0].detail.value.type, type);
+    });
+  });
+
+  describe('type change', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture();
+    });
+
+    it('should not update type if same as current', async () => {
+      const type = 'application/json';
+      const spy = sinon.spy();
+      element.addEventListener('type-changed', spy);
+      element.type = type
+      await aTimeout(100)
+
+      element._changeHandler({
+        detail: {
+          value: type
+        }
+      });
+      assert.equal(element.type, type);
+      assert.isTrue(spy.notCalled);
+    });
+
+    it('dispatches type-changed', async () => {
+      const type = 'text/plain';
+      const newType = 'application/json';
+      element.type = type
+      await aTimeout(100)
+
+      const spy = sinon.spy();
+      element.addEventListener('type-changed', spy);
+
+      element._changeHandler({
+        detail: {
+          value: newType
+        }
+      });
+      assert.equal(element.type, newType);
+      assert.isTrue(spy.called);
+    });
+
   });
 
   describe('_computeAccept()', () => {
